80b9e2ae8a6e9b03abd3f2c001ecd3a366bf2c71
lovetrivedi <lovelyjoshi93@gmail.com>
2020-07-10 18:53:11 +0530

    added video to background

--- src/pages/Home/Hero.js
+++ src/pages/Home/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { css } from "glamor";
 import {Box, Button, Grid} from '@material-ui/core'
 import s from '../../styles/styles.module.css'
 import Spacer from 'react-spacer'
@@ -9,13 +10,19 @@ import Results from './animations/results'
 
   export default function Hero() {
     const [spacing] = React.useState(8);
+
+    console.log(`require("./../../assets/herobg.mp4")`, require("./../../assets/herobg.mp4"))
   
    return (
       
-      <Box bgcolor="primary.dark" color="primary.contrastText" p={4} >
-       <Spacer height="10px"/>
-        <Grid container alignItems="center" justify="center">
-            <Grid item md={10} xs={12} style={{textAlign: 'left'}}>
+      <Box bgcolor="primary.dark" color="primary.contrastText" p={4} {...css(styles.container)}>
+          <video loop autoPlay {...css(styles.vidDiv)}>
+            <source src={require("./../../assets/herobg.mp4")} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+       <div {...css(styles.coverOpacity)}></div>
+        <Grid container alignItems="center" justify="center" {...css(styles.content)}>
+            <Grid item md={10} xs={12} style={{textAlign: 'left', "paddingTop": "80px"}}>
                    <h1  className={s.header} style={{textAlign: 'left'}}>
                     Artificial intelligence made simple
                     </h1>
@@ -32,3 +39,28 @@ import Results from './animations/results'
     </Box>  
   )
 }
+
+const styles = {
+  vidDiv:{
+    width: "100%",
+    position: "absolute",
+    left: 0,
+    top: "-80px"
+  },
+  container: {
+    overflow: "hidden",
+    position: "relative",
+    padding: "0px !important"
+  },
+  content: {
+    position: "relative",
+    padding: "32px 0px"
+  },
+  coverOpacity: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    backgroundColor: "black",
+    opacity: 0.7
+  }
+};
