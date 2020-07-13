b90c2e1a2cd9ba6bb4cfb6cd5560a74fa8a6b085
Jose <yo@josemathias.com>
2020-07-13 18:34:14 +1000

    update below;

--- src/pages/Home/Below.js
+++ src/pages/Home/Below.js
@@ -21,6 +21,7 @@ import Provision from '../../assets/provision.png'
               Our mission
               </h2>
                <p className={s.subheader} style={{textAlign: 'left'}}>Zeptron makes it easy for anyone to use computer vision without requiring technical skills.</p>
+               <p className={s.subheader} style={{textAlign: 'left'}}>Our platform is a business tool that makes the world better and more efficient for professionals of all kinds.</p>
                <p className={s.subheader} style={{textAlign: 'left'}}>We support the open and safe use of AI as a tool for the betterment of humankind.</p>
               <Spacer height="40px"/>
             </Grid> 
