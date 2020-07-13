fa35ee2b25cb391be647fab2295b7264afea4933
Jose <yo@josemathias.com>
2020-07-13 11:44:29 +1000

    update lp

--- src/pages/Streaming.js
+++ src/pages/Streaming.js
@@ -3,6 +3,7 @@ import {Box, Grid} from '@material-ui/core'
 import s from '../styles/styles.module.css'
 import Spacer from 'react-spacer'
 import Top from '../assets/top.jpg'
+import IPad from '../assets/ipad.png'
 
 export default function Streaming() {
 
@@ -21,14 +22,56 @@ export default function Streaming() {
             <Spacer height="100px"/>
             <Grid container alignItems="center" justify="center">
                 <Grid item md={8} xs={12}>
+                    <h2 className={s.header}>
+                        Zeptron Cloud-Connected Camera
+                    </h2>
                     <Grid container alignItems="center" justify="center">
-                    <Grid item md={6} xs={12}>
+                    <Grid item md={12} xs={12}>
                         <img style={{maxWidth: '50%'}} src={Top} />
                     </Grid>
                     </Grid>
+                    <Grid container alignItems="center" justify="center">
+                    <Grid item md={6} xs={12}>
+                        <h4 className={s.subheader}>
+                            Features
+                        </h4>
+                        <p  className={s.subclass}>
+                            1 x Ethernet 
+                        </p>
+                        <p  className={s.subclass}>
+                            802.11 bgn wireless
+                        </p>
+                        <p  className={s.subclass}>
+                            2 x USB 2.0 
+                        </p>
+                        <p  className={s.subclass}>
+                            2 x USB 3.0 
+                        </p>
+                        <p  className={s.subclass}>
+                            Tripod Mount
+                        </p>
+                        <p  className={s.subclass}>
+                            Active Fan Cooling
+                        </p>
+
+                    </Grid>
+                    </Grid>
+                    
                 </Grid>
             </Grid>
             <Spacer height="100px"/>
+            <Box bgcolor="primary.dark" color="primary.contrastText">
+            <Spacer height="100px"/>
+            <Grid container alignItems="center" justify="center">
+                <Grid item md={8}>
+                <h2 className={s.header}>
+                        Easy Browser-Based Configuration
+                    </h2>
+                    <img src={IPad} style={{maxWidth: '100%'}}/>
+                </Grid>
+            </Grid>
+            <Spacer height="100px"/>
+            </Box>
             </Box>
         </div>
     )
