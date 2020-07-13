3e45f1c3e082239e60648a13e734e05ead46f321
Jose <yo@josemathias.com>
2020-07-13 19:52:10 +1000

    update below

--- src/pages/Home/Below.js
+++ src/pages/Home/Below.js
@@ -22,7 +22,7 @@ import Provision from '../../assets/provision.png'
               </h2>
                <p className={s.subheader} style={{textAlign: 'left'}}>Zeptron makes it easy for anyone to use computer vision without requiring technical skills.</p>
                <p className={s.subheader} style={{textAlign: 'left'}}>Our platform makes work safer and more efficient for organisations and professionals of all kinds.</p>
-               <p className={s.subheader} style={{textAlign: 'left'}}>We support the open and safe use of AI as a tool for the betterment of humankind.</p>
+               <p className={s.subheader} style={{textAlign: 'left'}}>We are committed to using AI as a tool for the betterment of humankind.</p>
               <Spacer height="40px"/>
             </Grid> 
             
