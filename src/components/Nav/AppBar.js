f95d57c0dd25bd4571b4642d1f84563e78ac5a23
Jose <yo@josemathias.com>
2020-07-14 23:40:51 +1000

    fix access denied erorr

--- src/components/Nav/AppBar.js
+++ src/components/Nav/AppBar.js
@@ -87,7 +87,7 @@ export default function SwipeableTemporaryDrawer(props) {
     route: "/contact",
     icon: ""
   },{
-    name : "Sign-In",
+    name : "Login",
     route: "/auth",
     icon: ''
   }]
